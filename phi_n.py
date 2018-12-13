8c4f24be785d5dc125bdbd1057f4a479f72a00db
Christian Sumano <csumano@oswego.edu>
2018-12-13 14:55:50 -0500

    update phi n

--- phi_n.py
+++ phi_n.py
@@ -1,9 +1,11 @@
 import math as math
 import random
 
+#Testing a simple factorization method
 def factors(n):
     return set(reduce(list.__add__,([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
 
+#Euclidean to determine greatest common divsor
 def gcd(a, b):
     while b != 0:
         a, b = b, a % b
@@ -24,33 +26,57 @@ def modinv(e, phi):
         return x % phi
 
 def rsa(p, q):
+    #Compute n
     n = p * q
 
     g = factors(n)
-    print g
 
+    #Commpute phi
     phi = (p-1) * (q-1)
 
-    print "phi(n) = ", phi
-
+    #Choose encrypted exponenet, e
     e = random.randrange(1,phi)
 
+    #use Eucldiean to verify e and phi(n) are coprime
     g = gcd(e, phi)
     while g != 1:
         e = random.randrange(1, phi)
         g = gcd(e, phi)
 
+    # compute d
     d = modinv(e, phi)
 
-    print "Public key = (", e,n, ")"
-    print "Private key = (", d,n, ")"
-    print "-----------------------------"
+    return ((e, n), (d, n))
+
+def encrypt(public, m):
+    e, n = public
+    c = ((m**e) % n)
+    return c
+
+def decrypt(private, encrypted_message):
+    d, n = private
+    m1 = ((encrypted_message**d) % n)
+    return m1
 
 def main():
+
+    print "RSA Encrypter/ Decrypter"
     p = int(raw_input("Enter a prime number: "))
     q = int(raw_input("Enter a different prime number: "))
     print "-----------------------------"
-    rsa(p,q)
+    public, private = rsa(p, q)
+    print "Public key = ", public
+    # print "Private key = ", private
+    print "-----------------------------"
+    m = int(raw_input("Enter a message to encrypt: "))
+    encrypted_message = encrypt(public, m)
+    print "-----------------------------"
+    print "The encrypted message is: ", encrypted_message
+    print "-----------------------------"
+    print "Decrypting with public key..."
+    print "-----------------------------"
+    decrypted_message = decrypt(private, encrypted_message)
+    print "Decrypted message is: ", decrypted_message
 
 if __name__ == "__main__":
     main()
